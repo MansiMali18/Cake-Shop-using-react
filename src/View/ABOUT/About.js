86059fea63853b0bbfec3d65227dbab3b1cc38b3
MansiMali18 <mansimali399@gmail.com>
2024-08-14 12:03:38 +0530

    added basic code in about us page

--- src/View/ABOUT/About.js
+++ src/View/ABOUT/About.js
@@ -1,19 +1,28 @@
 
 import Navbar from "../../Components/Navbar.js"
-import AboutImg from "./About us page-pana.png"
 import "./../../index.js"
 import "./about.css"
+import AboutImg from "./about-img.PNG"
 function About() 
 {
   return (
     <>
     <Navbar/>
-    <div className='about'>
-      <div className="headings">
-      <span className='heading'>About</span>
-      <img src={AboutImg} className='heading-img'/>
-      </div>
-    </div>
+    <div className='about-container'>
+      <div className="about-headings">
+          <img src={AboutImg} className="about-heading-img"/>
+          *****&emsp;&emsp;The history of our sweets !!!&emsp;&emsp;*****
+          <img src={AboutImg} className="about-heading-img"/>
+      </div> 
+      </div> 
+      <div className="section-1">
+        <div className="left-img">
+        <img src="https://www.fnp.com/images/pr/l/v20220706181812/luxe-love-orchids-bouquet-truffle-cake_1.jpg" className="about-img"/>
+        </div>
+        <div className="right-info">
+          
+        </div>
+      </div>    
     </>
     )
 }
