79704dec558cccf04d126a5f98a9f28b10ad2d49
MansiMali18 <mansimali399@gmail.com>
2024-08-31 20:55:21 +0530

    used map function and make array for item

--- src/View/SERVICES/Services.js
+++ src/View/SERVICES/Services.js
@@ -2,6 +2,106 @@ import Cards from "./../../Components/cards.js"
 import React from 'react'
 import Navbar from '../../Components/Navbar';
 import "./services.css"
+
+const cakes=[
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v20221221172007/decorated-chocolate-truffle-cake-half-kg_1.jpg",
+    title:"Decorated Chocolate Truffle Cake Half Kg",
+    description:"Rs. 575"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v20221214202641/chocolate-truffle-cream-cake-half-kg_1.jpg",
+          title:"Chocolate Truffle Delicious Cake Half Kg",
+          description:"Rs. 599"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v20221205202945/fudge-brownie-cake-half-kg_1.jpg",
+    title:"Fudge Brownie Cake Half kg",
+    description:"Rs. 675"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v200/chocolate-cream-cake-half-kg_1.jpg",
+    title:"Fudge Brownie Cake Half kg",
+    description:"Rs. 675"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v20221205202945/fudge-brownie-cake-half-kg_1.jpg",
+    title:"Chocolate Cream Cake Half kg",
+    description:"Rs. 599"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v20221214202651/chocolate-walnut-truffle-half-kg_1.jpg",
+    title:"Chocolate Walnut Truffle Cake- Half kg",
+    description:"Rs.755"
+  }
+]
+const cakes2=[
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v20221107183531/princess-theme-strawberry-cake-eggless-2-kg_1.jpg",
+          title:"Princess Theme Strawberry Cake Eggless 2 Kg",
+          description:"Rs.2375"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v200/pink-strawberry-cream-cake-1-kg-eggless_1.jpg",
+    title:"Pink Strawberry Cream Cake 1 Kg Eggless",
+    description:"Rs.1175"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v200/in-love-strawberry-cake-1-kg_1.jpg",
+    title:"In Love Strawberry Cake 1 Kg",
+    description:"Rs.1475"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v200/cream-drop-black-forest-cake-for-mom-1-kg_1.jpg",
+    title:"Cream Drop Strawberry cake 1 Kg",
+    description:"Rs.1375"
+  }
+]
+const cakes3=[
+  {
+  ImgURL:"https://www.fnp.com/images/pr/l/v200/pineapple-cake-half-kg-eggless_1.jpg",
+  title:"Pineapple Cake Half kg Eggless",
+  description:"Rs.549"
+
+  },
+  { 
+    ImgURL:"https://www.fnp.com/images/pr/l/v200/fruit-overload-cake-half-kg_1.jpg",
+          title:"Fruit Overload Cake Half Kg",
+          description:"Rs.725"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v200/happy-emoji-pineapple-cake-half-kg_1.jpg",
+    title:"Happy Emoji Pineapple Cake Half Kg",
+    description:"Rs.625"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v200/tropical-bliss-pineapple-bento_1.jpg",
+    title:"Tropical Bliss Pineapple Bento 250 Gram",
+    description:"Rs.449"
+  }
+]
+const cakes4=[
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v20190529204632/frosty-mug-designer-truffle-cake-1-kg-eggless_1.jpg",
+    title:"Frosty Mug Designer Truffle Cake 1 Kg Eggless",
+    description:"Rs.1275"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v200/eggless-decadent-duo-of-choco-chip-n-black-forest-jar-cakes_1.jpg",
+    title:"Eggless Decadent Duo of Choco Chip & Black Forest Jar Cakes",
+    description:"Rs.525"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v20221205202102/black-forest-cream-cake-jar-eggless-set-of-2_1.jpg",
+    title:"Black Forest Cream Cake Jar Eggless",
+     description:"Rs.575"
+  },
+  {
+    ImgURL:"https://www.fnp.com/images/pr/l/v200/divine-black-forest-eggless-cake-half-kg_1.jpg",
+    title:"Divine Black Forest Eggless Cake- Half Kg",
+    description:"Rs.649"
+  }
+]
 function Services() {
   return (
     <div>
@@ -10,111 +110,49 @@ function Services() {
         *****&emsp;&emsp;Chocolate Flavour&emsp;&emsp;*****
       </div>
       <div className="cards-main-container">
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v20221221172007/decorated-chocolate-truffle-cake-half-kg_1.jpg"
-          title="Decorated Chocolate Truffle Cake Half Kg"
-          description="Rs. 575"
+        {cakes.map((cakeitem)=>{
+        return <Cards
+          ImgURL={cakeitem.ImgURL}
+          title={cakeitem.title}
+          description={cakeitem.description}
         />
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v20221214202641/chocolate-truffle-cream-cake-half-kg_1.jpg"
-          title="Chocolate Truffle Delicious Cake Half Kg"
-          description="Rs. 599"
-        />
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v20221205202945/fudge-brownie-cake-half-kg_1.jpg"
-          title="Fudge Brownie Cake Half kg"
-          description="Rs. 675"
-        />
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/chocolate-cream-cake-half-kg_1.jpg"
-          title="Fudge Brownie Cake Half kg"
-          description="Rs. 675"
-        />
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v20221205202945/fudge-brownie-cake-half-kg_1.jpg"
-          title="Chocolate Cream Cake Half kg"
-          description="Rs. 599"
-        />
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v20221214202651/chocolate-walnut-truffle-half-kg_1.jpg"
-          title="Chocolate Walnut Truffle Cake- Half kg"
-          description="Rs.755"
-        />
+        })}
       </div>
       <div className="cake-heading" >
         *****&emsp;&emsp;Strawberry Flavour&emsp;&emsp;*****
       </div>
       <div className="cards-main-container">
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v20221107183531/princess-theme-strawberry-cake-eggless-2-kg_1.jpg"
-          title="Princess Theme Strawberry Cake Eggless 2 Kg"
-          description="Rs.2375"
+      {cakes2.map((cakeitem)=>{
+        return <Cards
+          ImgURL={cakeitem.ImgURL}
+          title={cakeitem.title}
+          description={cakeitem.description}
         />
-       <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/pink-strawberry-cream-cake-1-kg-eggless_1.jpg"
-          title="Pink Strawberry Cream Cake 1 Kg Eggless"
-          description="Rs.1175"
-        />
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/in-love-strawberry-cake-1-kg_1.jpg"
-          title="In Love Strawberry Cake 1 Kg"
-          description="Rs.1475"
-        />
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/cream-drop-black-forest-cake-for-mom-1-kg_1.jpg"
-          title="Cream Drop Strawberry cake 1 Kg"
-          description="Rs.1375"
-        />
+        })}
       </div>
       <div className="cake-heading" >
         *****&emsp;&emsp;Pineapple Flavour&emsp;&emsp;*****
       </div>
       <div className="cards-main-container">
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/pineapple-cake-half-kg-eggless_1.jpg"
-          title="Pineapple Cake Half kg Eggless"
-          description="Rs.549"
+      {cakes3.map((cakeitem)=>{
+        return <Cards
+          ImgURL={cakeitem.ImgURL}
+          title={cakeitem.title}
+          description={cakeitem.description}
         />
-       <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/fruit-overload-cake-half-kg_1.jpg"
-          title="Fruit Overload Cake Half Kg"
-          description="Rs.725"
-        />
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/happy-emoji-pineapple-cake-half-kg_1.jpg"
-          title="Happy Emoji Pineapple Cake Half Kg"
-          description="Rs.625"
-        />
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/tropical-bliss-pineapple-bento_1.jpg"
-          title="Tropical Bliss Pineapple Bento 250 Gram"
-          description="Rs.449"
-        />
+        })}
       </div>
       <div className="cake-heading" >
         *****&emsp;&emsp;Black Forest Flavour&emsp;&emsp;*****
       </div>
       <div className="cards-main-container">
-        <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v20190529204632/frosty-mug-designer-truffle-cake-1-kg-eggless_1.jpg"
-          title="Frosty Mug Designer Truffle Cake 1 Kg Eggless"
-          description="Rs.1275"
+         {cakes4.map((cakeitem)=>{
+        return <Cards
+          ImgURL={cakeitem.ImgURL}
+          title={cakeitem.title}
+          description={cakeitem.description}
         />
-         <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/eggless-decadent-duo-of-choco-chip-n-black-forest-jar-cakes_1.jpg"
-          title="Eggless Decadent Duo of Choco Chip & Black Forest Jar Cakes"
-          description="Rs.525"
-        />
-         <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v20221205202102/black-forest-cream-cake-jar-eggless-set-of-2_1.jpg"
-          title="Black Forest Cream Cake Jar Eggless"
-           description="Rs.575"
-        />
-         <Cards
-          ImgURL="https://www.fnp.com/images/pr/l/v200/divine-black-forest-eggless-cake-half-kg_1.jpg"
-          title="Divine Black Forest Eggless Cake- Half Kg"
-          description="Rs.649"
-        />
+        })}
         </div>
     </div>
 
